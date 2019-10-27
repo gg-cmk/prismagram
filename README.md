a9c07ff45ee0f8c21a9b950bff77f55dfaba71cc
username <user@email.mail>
2019-10-27 09:06:15 +0900

    updates done by...

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # prismagram
+
 Instagram Clone
+
+## User Stories
+
+- [ ] See the feed
+- [X] Like / Unlike a photo
+- [X] Comment on a photo
+- [X] Search by user
+- [X] Search by location
+- [X] See user profile
+- [X] See My profile
+- [X] Follow / Unfollow User
+- [X] See the Full Photo
+- [X] Edit my profile
+- [X] Create account
+- [ ] Log in
+- [ ] Upload Photos
+- [ ] Edit the photo (Delete)
+- [ ] See the Feed
+- [X] request Secret
+- [X] Confirm Secret 
---
+++ src/api/User/User.js
@@ -0,0 +1,58 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    posts: ({ id }) => prisma.user({ id }).posts(),
+    following: ({ id }) => prisma.user({ id }).following(),
+    followers: ({ id }) => prisma.user({ id }).followers(),
+    likes: ({ id }) => prisma.user({ id }).likes(),
+    comments: ({ id }) => prisma.user({ id }).comments(),
+    rooms: ({ id }) => prisma.user({ id }).posts(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({
+          where: {
+            followers_some: { id }
+          }
+        })
+        .aggregate()
+        .count(),
+    followersCount: ({ id }) =>
+      prisma
+        .usersConnection({
+          where: {
+            followers_some: { id }
+          }
+        })
+        .aggregate()
+        .count(),
+    fullName: parent => {
+      retrn`${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              followers_some: {
+                id: parentId
+              }
+            }
+          ]
+        });
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
